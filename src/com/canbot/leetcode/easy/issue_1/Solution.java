663e278d82c96207e59066a2f367bca8a5defbb5
liutao <tao.liu@uurobot.cn>
2018-06-01 14:15:55 +0800

    Initial commit

---
+++ src/com/canbot/leetcode/easy/issue_1/Solution.java
@@ -0,0 +1,81 @@
+package com.canbot.leetcode.easy.issue_1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by tao.liu on 2018/5/31.
+ */
+public class Solution {
+
+	/**
+	 * 复杂度分析：
+	 * 时间复杂度：O(n^2)， 对于每个元素，我们试图通过遍历数组的其余部分来寻找它所对应的目标元素，
+	 * 这将耗费 O(n) 的时间。因此时间复杂度为 O(n^2)。
+	 * 空间复杂度：O(1)。
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[j] == target - nums[i]) {
+					return new int[]{i, j};
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	/**
+	 * 时间复杂度：O(n)， 我们把包含有 nn 个元素的列表遍历两次。由于哈希表将查找时间缩短到 O(1) ，
+	 * 所以时间复杂度为 O(n)。
+	 * 空间复杂度：O(n)， 所需的额外空间取决于哈希表中存储的元素数量，该表中存储了 nn 个元素。
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSum2(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement) && map.get(complement) != i) {
+				return new int[]{i, map.get(complement)};
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	/**
+	 * 时间复杂度：O(n)， 我们只遍历了包含有 nn 个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。
+	 *
+	 * 空间复杂度：O(n)， 所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 n 个元素。
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSum3(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[]{map.get(complement), i};
+			}
+			map.put(nums[i], i);
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[] nums = new int[]{2, 7, 11, 15};
+		int target = 18;
+		System.out.println(Arrays.toString(solution.twoSum2(nums, target)));
+	}
+
+}
